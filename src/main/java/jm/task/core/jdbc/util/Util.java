d4133ea72ba97e6ea3f086211756c6578a0cea4c
hammondy <hammondy@student.21-school.ru>
2023-03-18 10:56:14 +0700

    connect JDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306";
+    public static final String DB_USER = "user";
+    public static final String DB_PASSWD = "1234";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
