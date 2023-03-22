bfe6efd85b8263aa2176ccbead58de95430e72d3
hammondy <hammondy@student.21-school.ru>
2023-03-22 19:08:59 +0700

    Utils

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,43 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if(!sessionFactory.isClosed()) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/users_db");
+                properties.put(Environment.USER, "user");
+                properties.put(Environment.PASS, "1234");
+                properties.put(Environment.DIALECT, "dialect");
+                properties.put(Environment.SHOW_SQL, true);
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (HibernateException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+    public static void close() {
+        sessionFactory.close();
+    }
 }
